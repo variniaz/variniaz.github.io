46acff31fc7b1515275f6b61dfc849bcfe97d4eb
varin <variniazkarin@gmail.com>
2026-02-02 19:51:57 +0700

    main: restore config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,31 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+
 import tailwindcss from "@tailwindcss/vite";
+
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
+import keystatic from "@keystatic/astro";
+import vercel from "@astrojs/vercel";
+
 import icon from "astro-icon";
 
-// only import keystatic conditionally
-const integrations = [
-  react(),
-  markdoc(),
-  icon(),
-];
-
-if (!import.meta.env.PROD) {
-  const { default: keystatic } = await import("@keystatic/astro");
-  integrations.push(keystatic());
-}
-
+// https://astro.build/config
 export default defineConfig({
-  output: "static",
-  integrations,
+  integrations: [react(), markdoc(), keystatic(), icon()],
+
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
       include: ["@keystatic/core", "@keystatic/astro"],
     },
   },
-  site: "https://variniaz.github.io",
-  base: "/",
+
+  output: "server",
+
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
